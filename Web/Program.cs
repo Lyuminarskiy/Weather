9ce66ddd37e2aeabf5c6f4157ffb1354dc5e36f5
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2020-11-05 20:48:00 +0400

    - Вынос общей разметки в Layout. - Обработка ошибок.

--- Web/Program.cs
+++ Web/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Web
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -12,7 +12,7 @@ namespace Web
                 .Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
