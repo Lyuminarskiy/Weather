1f4ed8d2ae17218b7c88d61c078bd146ac8c27ad
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2020-11-21 11:25:52 +0400

    Добавлена документация для функций сервиса погоды.

--- Shared/WeatherService.cs
+++ Shared/WeatherService.cs
@@ -9,7 +9,21 @@ namespace Shared
         private static readonly IOpenWeatherMapApi OpenWeatherMapApi = RestService
             .For<IOpenWeatherMapApi>(BaseUrl);
         
-        public static Task<WeatherData> GetWeather(string city) => OpenWeatherMapApi.GetWeather(city);
+        /// <summary>
+        /// Получить погоду для города <paramref name="city"/>.
+        /// </summary>
+        /// <param name="city">имя города.</param>
+        /// <returns>Данные о погоде.</returns>
+        /// <seealso cref="https://openweathermap.org/current#name"/>
+        public static Task<WeatherData> GetWeather(string city) => OpenWeatherMapApi.GetWeather(city.Trim());
+        
+        /// <summary>
+        /// Получить погоду по географическим координатам.
+        /// </summary>
+        /// <param name="latitude">широта.</param>
+        /// <param name="longitude">долгота.</param>
+        /// <returns>Данные о погоде.</returns>
+        /// <seealso cref="https://openweathermap.org/current#geo"/>
         public static Task<WeatherData> GetWeather(double latitude, double longitude) => 
             OpenWeatherMapApi.GetWeather(latitude, longitude);
     }
